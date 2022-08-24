18fa586b8c0e510afd45ee276b393cd67b9a762f
skynetpanda9 <akashpandya18@outlook.com>
2022-08-24 13:22:13 +0530

    fix patch service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,32 +8,38 @@ import {
   Patch,
 } from '@nestjs/common';
 import { Request } from 'express';
+import { UserService } from './user.service';
 
 @Controller('/user')
 export class UserController {
+  //   private userService;
+  //   constructor() {
+  //     this.userService = new UserService();
+  //   }
+  constructor(private userService: UserService) {}
+
   @Get()
   getUsers() {
-    return { name: 'Akash', email: 'test@user.com' };
+    return this.userService.get();
   }
 
   @Post()
   store(@Req() req: Request) {
-    // console.log(req.body);
-    return req.body;
+    return this.userService.create(req);
   }
 
-  @Patch()
-  update(@Req() req: Request) {
-    return req.body;
+  @Patch('/:userId')
+  update(@Req() req: Request, @Param() param: { userId: number }) {
+    return this.userService.update(req, param);
   }
 
   @Get('/:userId')
-  getUser(@Param() params: { userId: number }) {
-    return params;
+  getUser(@Param() param: { userId: number }) {
+    return this.userService.show(param);
   }
 
   @Delete('/:userId')
-  deleteUser(@Param() params: { userId: number }) {
-    return params;
+  deleteUser(@Param() param: { userId: number }) {
+    return this.userService.delete(param);
   }
 }
